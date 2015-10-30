588b57850299fbf3c0a941b9d20111e58c85f7ea
Aulia Agung Maulana <auliaagungmaulana@d-i89-244-110.student.eduroam.uq.edu.au>
2015-10-30 16:25:04 +1000

    Added contents and images

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,15 +1,26 @@
 
 /* Background Images
 -------------------------------------------------------------------*/
-var  pageTopImage = jQuery('#page-top').data('background-image');
-var  aboutImage = jQuery('#about').data('background-image');
-var  subscribeImage = jQuery('#subscribe').data('background-image');
-var  contactImage = jQuery('#contact').data('background-image');
+var pageTopImage = jQuery('#page-top').data('background-image');
+var aboutImage = jQuery('#about').data('background-image');
+var subscribeImage = jQuery('#subscribe').data('background-image');
+var contactImage = jQuery('#contact').data('background-image');
+var firstImage = jQuery('#first').data('background-image');
+var secondImage = jQuery('#second').data('background-image');
+var thirdImage = jQuery('#third').data('background-image');
+var ourTeamImage = jQuery('#ourteam').data('background-image');
+var problemsImage = jQuery('#problems').data('background-image');
 
 if (pageTopImage) {  jQuery('#page-top').css({ 'background-image':'url(' + pageTopImage + ')' }); };
 if (aboutImage) {  jQuery('#about').css({ 'background-image':'url(' + aboutImage + ')' }); };
 if (subscribeImage) {  jQuery('#subscribe').css({ 'background-image':'url(' + subscribeImage + ')' }); };
 if (contactImage) {  jQuery('#contact').css({ 'background-image':'url(' + contactImage + ')' }); };
+if (firstImage) {  jQuery('#first').css({ 'background-image':'url(' + firstImage + ')' }); };
+if (secondImage) {  jQuery('#second').css({ 'background-image':'url(' + secondImage + ')' }); };
+if (thirdImage) {  jQuery('#third').css({ 'background-image':'url(' + thirdImage + ')' }); };
+if (ourTeamImage) {  jQuery('#ourteam').css({ 'background-image':'url(' + ourTeamImage + ')' }); };
+if (problemsImage) {  jQuery('#problems').css({ 'background-image':'url(' + problemsImage + ')' }); };
+
 
 /* Background Images End
 -------------------------------------------------------------------*/
@@ -210,27 +221,5 @@ jQuery(document).ready(function($) {
 
 	/* Contact End
 	-------------------------------------------------------------------*/
-
-
-    
-
-    
-
-
-
 });
-
-/* Document Ready function End
--------------------------------------------------------------------*/
-
-
-/* Preloder 
--------------------------------------------------------------------*/
-$(window).load(function () {    
-    "use strict";
-    $("#loader").fadeOut();
-    $("#preloader").delay(350).fadeOut("slow");
-});
- /* Preloder End
--------------------------------------------------------------------*/
-   
+ 
